7d92cc3507ab69bf6859a880409832feb95f1f61
Konstantinos Perperidis <k.a.perperidis@gmail.com>
2025-04-08 23:13:16 +0300

    WIP: backup commit

--- R-sources/ppmi-stratified/Main.R
+++ R-sources/ppmi-stratified/Main.R
@@ -2,29 +2,115 @@ library(DESeq2)
 
 setwd('/Users/kpax/Documents/aep/study/MSC/lab/PPMI_Project_133_RNASeq')
 
+# cell_props = pd.read_csv("/Users/kpax/Documents/aep/study/MSC/lab/PPMI_Project_133_RNASeq/cibersortx_metadata_df.csv", index_col=0)
+# filtered_cell_meta = filtered_cell_meta.loc[:, filtered_cell_meta.columns.isin(["Diagnosis", "Neutrophils", "Monocytes", "T cells CD4 naive", "NK cells activated", "NK cells resting", "T cells CD4 memory resting"])]
+# filtered_cell_meta = filtered_cell_meta.loc[:, filtered_cell_meta.sum(axis=0) != 0]
+# design_factors = filtered_cell_meta.columns.values.tolist()
+# 
+
 counts <- read.csv("counts.csv", row.names = 1)
 metadata <- read.csv("metadata.csv", row.names = 1)
+cell_props <- read.csv("cibersortx_metadata_df.csv", row.names=1)
 
-metadata_filtered <- subset(metadata, Visit == "BL" & Diagnosis %in% c("PD", "Control"))
-counts_filtered <- counts[rownames(metadata_filtered),]
 
-BiocManager::install("sva")
-library(sva)
+rownames(metadata)
 
-mod <- model.matrix(~ Diagnosis + Gender + Age_Group, metadata_filtered)
-mod0 <- model.matrix(~ Gender + Age_Group, metadata)
+cell_props <- cell_props %>% select(where(~ is.numeric(.) && sum(., na.rm=TRUE) > 0))
+cell_props <- cell_props[, !(names(cell_props) %in% c("RMSE", "Correlation"))]
+metadata <- cbind(metadata, cell_props[row.names(metadata), ])
 
-svaseq <- svaseq(t(counts_filtered), mod, mod0)
-metadata$SV1 <- svseq$sv[, 1]
-metadata$SV2 <- svseq$sv[, 2]
+design_full <- "T.cells.CD4.memory.resting + T.cells.CD4.naive + NK.cells.resting + Monocytes + Neutrophils + Diagnosis"
 
-dds <- DESeqDataSetFromMatrix(
-  countData = t(counts_filtered),
-  colData = metadata_filtered,
-  design = ~ Gender + Age_Group + Diagnosis + Gender:Diagnosis + Age_Group:Diagnosis
+design_reduced <- "Monocytes + Neutrophils + Diagnosis"
+
+strata <- list(
+  list(Gender="Male", Age_Group="30-50", Visit="BL", Diagnosis=c("PD", "Control"), Design=design_full),
+  list(Gender="Male", Age_Group="50-70", Visit="BL", Diagnosis=c("PD", "Control"), Design=design_full),
+  list(Gender="Male", Age_Group="70-80", Visit="BL", Diagnosis=c("PD", "Control"), Design=design_full),
+  list(Gender="Male", Age_Group=">80", Visit="BL", Diagnosis=c("PD", "Control"), Design=design_reduced),
+  list(Gender="Female", Age_Group="30-50", Visit="BL", Diagnosis=c("PD", "Control"), Design=design_full),
+  list(Gender="Female", Age_Group="50-70", Visit="BL", Diagnosis=c("PD", "Control"), Design=design_full),
+  list(Gender="Female", Age_Group="70-80", Visit="BL", Diagnosis=c("PD", "Control"), Design=design_full),
+  list(Gender="Female", Age_Group=">80", Visit="BL", Diagnosis=c("PD", "Control"), Design="Diagnosis")
 )
 
-dds <- DESeq(dds)
-res_main <- results(dds, name = "Diagnosis_PD_vs_Control")
-res_gender <- results(dds, contrast = list(c("Diagnosis_PD_vs_Control", "GenderMale.DiagnosisPD")))
-res_age50_70 <- results(dds, contrast = list(c("Diagnosis_PD_vs_Control", "Age_Group50-70.DiagnosisPD")))
+for (stratum in strata) {
+  meta_stratum <- metadata %>% filter(Gender == stratum$Gender,
+                                      Age_Group == stratum$Age_Group,
+                                      Visit == stratum$Visit,
+                                      Diagnosis %in% stratum$Diagnosis)
+  n_samples <- nrow(meta_stratum)
+  n_coeffs <- 8
+  if (n_samples <= n_coeffs) {
+    print(paste("For stratum ", stratum$Age_Group,", ",  stratum$Gender," there are ", n_samples, " samples but ", n_coeffs, " coefficients."))
+  }
+}
+
+results_list <- lapply(strata, function(stratum) {
+  print(paste("design=",stratum$Design))
+  meta_stratum <- metadata %>% filter(Gender == stratum$Gender,
+                                      Age_Group == stratum$Age_Group,
+                                      Visit == stratum$Visit,
+                                      Diagnosis %in% stratum$Diagnosis)
+  counts_stratum <- counts[rownames(meta_stratum), ]
+  dds <- DESeqDataSetFromMatrix(
+    t(counts_stratum),
+    meta_stratum,
+    design = formula(paste("~ ", stratum$Design))
+  )
+  dds <- DESeq(dds)
+  (dds)
+})
+results_list
+
+library(tibble)
+for (i in seq_along(results_list)) {
+  stratum_name <- ifelse(is.null(names(results_list)),
+                         paste0("Stratum_", i),
+                         names(results_list)[i])
+  res <- results(results_list[[i]], contrast=c("Diagnosis", "PD", "Control")) %>%
+    as.data.frame() %>%
+    rownames_to_column("Gene") %>%
+    arrange(padj, desc(abs(log2FoldChange)))
+  filename <- file.path("./dge_stratified", paste0("DEGs_stratified_", stratum_name, ".csv"))
+  write.csv(res, file=filename, row.names=FALSE)
+}
+
+names(results_list) <- sapply(strata, function(s) paste(s$Gender, s$Age_Group, sep="_"))
+results_list
+BiocManager::install("metaRNASeeq")
+library(metaRNASeq)
+pvals <- sapply(results_list, function(x) x$pvalue)
+log2FCs <- sapply(results_list, function(x) x$log2FoldChange)
+combined_pvals <- fishercomb(pvals)
+
+#metadata_filtered <- subset(metadata, Visit == "BL" & Diagnosis %in% c("PD", "Control") & Age_Group == "50-70" & Gender == "Male")
+#counts_filtered <- counts[rownames(metadata_filtered),]
+#head(metadata_filtered)
+#dim(metadata_filtered)
+#head(cell_props)
+#dim(cell_props)
+
+# library(dplyr)
+# cell_props <- cell_props %>% select(where(~ is.numeric(.) && sum(., na.rm=TRUE) > 0))
+# dim(cell_props)
+# 
+# cell_props <- cell_props[, !(names(cell_props) %in% c("RMSE", "Correlation"))]
+# colnames(cell_props)
+# 
+# metadata_with_cell_props <- cbind(metadata_filtered, cell_props[row.names(metadata_filtered), ])
+# 
+# dds <- DESeqDataSetFromMatrix(
+#   countData = t(counts_filtered),
+#   colData = metadata_with_cell_props,
+#   design = ~ T.cells.CD4.memory.resting + 
+#              T.cells.CD4.naive +
+#              NK.cells.activated + 
+#              NK.cells.resting +
+#              Monocytes + 
+#              Neutrophils +
+#              Diagnosis  
+#   )
+# 
+# dds <- DESeq(dds)
+# res <- results(dds, contrast=c("Diagnosis", "PD", "Control"))
---
+++ README.md
@@ -0,0 +1,3 @@
+# MSc Thesis on Functional Genetic Networks in Parkinson's Disease 🧬 🩸 
+___ 
+The thesis shall investigate on functional gene networks in Parkinson's Disease.
